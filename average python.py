e9911d2dce048d17f12489e08f22ccd510f62416
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2020-09-24 17:55:43 -0400

    Add files via upload

---
+++ average python.py
@@ -0,0 +1,13 @@
+numberofscores = 0
+score1 = 0
+score2 = 0
+total = 0
+average = 0.0
+scorecount = 0
+#numberofscores = int(input("please enter number of scores you want to average: "))
+score = int(input("please enter a test score: "))
+score = int(input("please enter a test score: "))
+average = ( score1 + score2 ) / 2
+print(average)
+
+
