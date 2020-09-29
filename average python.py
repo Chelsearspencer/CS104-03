5de39aaf3e6d6c15b0fb60ad0533b50e753fcd67
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2020-09-29 17:54:23 -0400

    Average python without loop

--- average python.py
+++ average python.py
@@ -4,10 +4,14 @@ score2 = 0
 total = 0
 average = 0.0
 scorecount = 0
-#numberofscores = int(input("please enter number of scores you want to average: "))
-score1 = int(input("please enter a test score: "))
-score2 = int(input("please enter a test score: "))
-average = ( score1 + score2 ) / 2
+numberofscores = int(input("please enter number of scores you want to average: "))
+
+#what python loop structure could we use to repeat the next 3 lines
+score = int(input("please enter a test score: "))
+total = total + score
+scorecount = scorecount + 1
+    
+average = total / numberofccores
 print(average)
 
 
