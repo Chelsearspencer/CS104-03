5586b5c6c2185aa686577121d01f67fc8e94a426
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2020-09-29 16:51:26 -0400

    Update average python.py

--- average python.py
+++ average python.py
@@ -5,8 +5,8 @@ total = 0
 average = 0.0
 scorecount = 0
 #numberofscores = int(input("please enter number of scores you want to average: "))
-score = int(input("please enter a test score: "))
-score = int(input("please enter a test score: "))
+score1 = int(input("please enter a test score: "))
+score2 = int(input("please enter a test score: "))
 average = ( score1 + score2 ) / 2
 print(average)
 
