9b5a92187850d7efd2f68ffe472543b2f7322426
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2020-09-29 21:06:06 -0400

    python with loop

--- average python.py
+++ average python.py
@@ -1,17 +1,19 @@
-numberofscores = 0
-score1 = 0
-score2 = 0
+numberOfScores = 0
+score = 0
 total = 0
 average = 0.0
-scorecount = 0
-numberofscores = int(input("please enter number of scores you want to average: "))
+scoreCount = 0
+numberOfScores = int(input("please enter number of scores you want to average: "))
 
 #what python loop structure could we use to repeat the next 3 lines
-score = int(input("please enter a test score: "))
-total = total + score
-scorecount = scorecount + 1
+while numberOfScores > scoreCount:
+    score = int(input("please enter a test score: "))
+    total = total + score
+    scoreCount = scoreCount + 1 
+    if numberOfScores == scoreCount:
+        break
     
-average = total / numberofccores
+average = (total) / numberOfScores
 print(average)
 
 
