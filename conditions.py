ad51bb9d2179222c0178f4c9c002ecb754c7df47
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2020-10-02 15:50:16 -0400

    conditions temp file

---
+++ conditions.py
@@ -0,0 +1,20 @@
+temp = 0
+temp = int(input("Please enter the current temperature : "))
+
+if temp > 90 :
+    print ("Wear Shorts")
+    
+if temp > 70 :
+    print ("Short sleeves are fine")
+    
+if temp > 50 :
+    print ("Wear a jacket")
+    
+if temp > 32 :
+    print ("Wear a heavy coat")
+    
+else :
+    print ("Stay inside")
+
+
+    
